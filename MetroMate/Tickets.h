2ec0f015e99e40f437eeb5da0771d9c683f8265f
Shahd-S-AbdAlaziz <2022170206@cis.asu.edu.eg>
2024-04-22 18:23:36 +0200

    Add project files.

---
+++ MetroMate/Tickets.h
@@ -0,0 +1,20 @@
+//#pragma once
+//#include<iostream>
+//#include <ctime>
+//using namespace std;
+//class Tickets {
+//	string id;
+//	string checkInStation;
+//	string checkOutStation;
+//	float fare;
+//	//time departureTiming;
+//	//time timeentryTiming;
+//
+//public:
+//	Tickets();
+//	Tickets(string, string, string);
+//	//char* checkInDT = time(0);
+//	//char* checkOutDT = time(0);
+//	float calculateFareForIndividualRides(string metroStages);
+//}
+//
