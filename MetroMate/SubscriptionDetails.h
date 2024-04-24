c7b2c9c18a52f16362f05066d280d70f3e5138cd
shimaa404 <2022170213@cis.asu.edu.eg>
2024-04-24 19:56:52 +0200

    subscription class,function in user,subscription main

---
+++ MetroMate/SubscriptionDetails.h
@@ -0,0 +1,43 @@
+#pragma once
+#include<string>
+#include<unordered_map>
+//#include "User.h"
+using namespace std;
+class SubscriptionDetails
+{
+public:
+	string name = "";
+	double price = 0;
+	int valid_duration = 0;/*
+	string firstDestination;
+	string targetDestination;*/
+	int cashAmount = 0;//for cash wallet only(multiply of 10)
+	int stagesPrices[4];
+	int stage = 0;
+	int path = 0;//to be in the data of user
+	int numberOfTrips = 0;
+
+public:
+	SubscriptionDetails(double price, int valid_duration, int cashAmount = 0,
+		const int* stagesPrices = nullptr, int stage = 0, string name = "", int path = 0, int numberOfTrips = 0)
+		: price(price), valid_duration(valid_duration), cashAmount(cashAmount), stage(stage), name(name), path(path), numberOfTrips(numberOfTrips) {
+		// Copy values from the array pointed to by stagesPrices
+		for (int i = 0; i < 4; ++i) {
+			this->stagesPrices[i] = stagesPrices[i];
+		}
+	}
+	SubscriptionDetails() {
+		//just empty constructor
+		//don't delete it
+	}
+	void DisplayData(string name);
+	//void PurchaceSunscription(User user, unordered_map<std::string, SubscriptionDetails> subscription_plans, unordered_map<int, string> subscriptions_names);
+	void calcStage(int chosenPath);
+	double calcPrice(int chosenPath);
+	void displayFixedData();
+	void Create();
+	void Modify(unordered_map<string, SubscriptionDetails>& subscription_plans, unordered_map<int, string>& subscriptions_names);
+	void remove(unordered_map<string, SubscriptionDetails>& subscription_plans, unordered_map<int, string>& subscriptions_names);
+	string toString() const;
+};
+
--- MetroMate/UserAccount.h
+++ MetroMate/UserAccount.h
@@ -1,8 +1,10 @@
 #pragma once
-#include <iostream>
-#include <unordered_map>
+#include "SubscriptionDetails.h"
+#include<iostream>
+#include<unordered_map>
+#include <ctype.h>//lower case
+#include<string>
 using namespace std;
-#include <string>
 class UserAccount
 {
 public:
@@ -11,6 +13,15 @@ public:
 	string Name;
 	string Address;
 	int Phone;
+
+	SubscriptionDetails chosenSubscription;
+	int availableTrips;
+
+	void PurchaceSubscription(UserAccount& user, unordered_map<std::string, SubscriptionDetails> subscription_plans, unordered_map<int, string> subscriptions_names);
+
+	UserAccount() {
+		//dont delete it
+	}
 	UserAccount(string email, string pass);
 	UserAccount(string Email, string Password, string Name, string Address, int Phone);
 	bool Register(unordered_map<string, UserAccount>& users, UserAccount user);
