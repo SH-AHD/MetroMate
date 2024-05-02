04bfe14c9bbcc8ceba5edefc2f0b9fff8aa0d403
SH-AHD <127779043+SH-AHD@users.noreply.github.com>
2024-05-02 07:43:05 +0300

    Add findTrips method

--- MetroMate/station.cpp
+++ MetroMate/station.cpp
@@ -1,1 +1,9 @@
 #include"station.h"
+
+void Station::findTrips(system_clock::time_point tripTime) {
+    for (auto& trainPair : stationSchedule) {
+        if (trainPair.second.getTrainSchedule().getArrivalTime() == tripTime) {
+            cout << trainPair.second.getTrainID();
+        }
+    }
+}
